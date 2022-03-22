c8e5da0c32e016421c21f891c2c27986684f870a
Joohan Lee <joohan33@gmail.com>
2022-03-22 12:54:18 -0400

    Still working on dev toggle counter

--- FirefoxFeatureToggleAnalyzer/src/main/java/edu/uwaterloo/lee/joohan/DevToggleCounter.java
+++ FirefoxFeatureToggleAnalyzer/src/main/java/edu/uwaterloo/lee/joohan/DevToggleCounter.java
@@ -1,62 +1,88 @@
 package edu.uwaterloo.lee.joohan;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DevToggleCounter {
-    public static Map<Integer, Integer> getTotalOnEachRelease() {
-        Map<Integer, Integer> buildIdAndToggleCount = new HashMap<>();
+//    public static Map<Integer, Integer> getTotalOnEachRelease() {
+//        Map<Integer, Integer> buildIdAndToggleCount = new HashMap<>();
+//
+//        for (int i = 57; i < 99; i++) {
+//            final int countFromAll = countAllJs(i);
+//            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromAll);
+//            buildIdAndToggleCount.putIfAbsent(i, countFromAll);
+//
+//            final int countFromFirefox = countFirefoxJs(i);
+//            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromFirefox);
+//            buildIdAndToggleCount.putIfAbsent(i, countFromFirefox);
+//
+//            final int countFromMobile = countMobileJs(i);
+//            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromMobile);
+//            buildIdAndToggleCount.putIfAbsent(i, countFromMobile);
+//        }
+//
+//        for (int i = 61; i < 70; i++) {
+//            int count = countStaticPrefListH(i);
+//            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += count);
+//            buildIdAndToggleCount.putIfAbsent(i, count);
+//        }
+//
+//        for (int i = 70; i < 99; i++) {
+//            int count = countStaticPrefListYaml(i);
+//            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += count);
+//            buildIdAndToggleCount.putIfAbsent(i, count);
+//        }
+//
+//        return buildIdAndToggleCount;
+//    }
 
-        for (int i = 57; i < 99; i++) {
-            final int countFromAll = countAllJs(i);
-            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromAll);
-            buildIdAndToggleCount.putIfAbsent(i, countFromAll);
+//    private static int countAllJs(int buildId) {
+//
+//
+//
+//    }
+//
+//    private static int countFirefoxJs(int buildId) {
+//
+//
+//    }
+//    private static int countMobileJs(int buildId) {
+//
+//
+//    }
+//    private static int countStaticPrefListH(int buildId) {
+//
+//
+//    }
+//
+//    private static int countStaticPrefListYaml(int buildId) {
+//
+//
+//    }
 
-            final int countFromFirefox = countFirefoxJs(i);
-            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromFirefox);
-            buildIdAndToggleCount.putIfAbsent(i, countFromFirefox);
+    public static Set<String> getUniqueNightlyBuildLine(int buildId) {
+        String fileName = String.format("downloaded_files/build_%s/init/all.js", buildId);
 
-            final int countFromMobile = countMobileJs(i);
-            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += countFromMobile);
-            buildIdAndToggleCount.putIfAbsent(i, countFromMobile);
+        Set<String> result = new HashSet<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.toLowerCase().contains("nightly")) {
+                    result.add(line);
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
 
-        for (int i = 61; i < 70; i++) {
-            int count = countStaticPrefListH(i);
-            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += count);
-            buildIdAndToggleCount.putIfAbsent(i, count);
-        }
-
-        for (int i = 70; i < 99; i++) {
-            int count = countStaticPrefListYaml(i);
-            buildIdAndToggleCount.computeIfPresent(i, (key, val) -> val += count);
-            buildIdAndToggleCount.putIfAbsent(i, count);
-        }
-
-        return buildIdAndToggleCount;
-    }
-
-    private static int countAllJs(int buildId) {
-
-
-
-    }
-
-    private static int countFirefoxJs(int buildId) {
-
-
-    }
-    private static int countMobileJs(int buildId) {
-
-
-    }
-    private static int countStaticPrefListH(int buildId) {
-
-
-    }
-
-    private static int countStaticPrefListYaml(int buildId) {
-
-
+        return result;
     }
 }
--- FirefoxFeatureToggleAnalyzer/src/main/java/edu/uwaterloo/lee/joohan/Main.java
+++ FirefoxFeatureToggleAnalyzer/src/main/java/edu/uwaterloo/lee/joohan/Main.java
@@ -1,13 +1,26 @@
 package edu.uwaterloo.lee.joohan;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] argv) {
-        Map<Integer, Integer> countOnEachRelease = ToggleCounter.getTotalOnEachRelease();
+//        Map<Integer, Integer> countOnEachRelease = ToggleCounter.getTotalOnEachRelease();
+//        System.out.println(
+//                countOnEachRelease.entrySet()
+//                        .stream()
+//                        .sorted(Map.Entry.comparingByKey())
+//                        .collect(Collectors.toList())
+//        );
 
-        System.out.println(countOnEachRelease.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(Collectors.toList()));
+        Set<String> nightlybuildStrings = new HashSet<>();
+        for (int i = 57; i < 99; i++) {
+            nightlybuildStrings.addAll(DevToggleCounter.getUniqueNightlyBuildLine(i));
+        }
+
+        System.out.println(nightlybuildStrings);
 
 
 
